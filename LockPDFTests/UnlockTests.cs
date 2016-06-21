c371daa57da35ff5430c838fc4915f1578ae78b4
Jonas Borges <jgborges@gmail.com>
2016-06-21 15:05:45 -0300

    begin tests

---
+++ LockPDFTests/UnlockTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LockPDFTests
+{
+    [TestClass]
+    public class UnlockTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
